d172a663efce2ea663cfe38ed789c392c61893d4
Case X B [STUDENT] <browncx@limestone.on.ca>
2025-01-09 12:12:53 -0500

    added og code to the repo

---
+++ Game.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Game extends VBox{
+    public Game(){
+        setAlignment(Pos.CENTER);
+        
+    }
+}
